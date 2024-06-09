22986a1483e47567e91579a3b25d8801f5b8deb4
stanislavlia <stanislavliashkov@gmail.com>
2024-06-09 14:42:13 +0700

    started with ex00 in CP00

---
+++ ex00/hello_world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char **argv) 
+{
+    char *current_part;
+
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::ends;
+    for (int i = 1; i < argc; i++)
+    {
+        current_part = argv[i];
+        while (*current_part)
+        {
+            std::cout << (char) std::toupper(*current_part) << std::ends;
+            current_part++;
+        }
+        std::cout << " " << std::ends;
+    }
+    std::cout << std::endl;
+}
