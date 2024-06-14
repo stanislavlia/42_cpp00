9bda6dea1b478d08cc0f9d8085aa9757c0576324
stanislavlia <stanislavliashkov@gmail.com>
2024-06-14 11:17:32 +0700

    modified ex00 to use std::string

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,20 +2,16 @@
 
 int main(int argc, char **argv) 
 {
-    char *current_part;
+    std::string current_part;
 
     if (argc == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::ends;
     for (int i = 1; i < argc; i++)
     {
         current_part = argv[i];
-        while (*current_part)
-        {
-            std::cout << (char) std::toupper(*current_part) << std::ends;
-            current_part++;
-        }
-        if (i < argc - 1)
-            std::cout << " " << std::ends;
+        for (int j = 0; j < current_part.length(); j++)
+            std::cout << (char) std::toupper(current_part[j]);
+        std::cout << ' ';
     }
     std::cout << std::endl;
     return (0);
